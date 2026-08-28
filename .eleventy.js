1f2b80a5d36b85487b198d6caf176ff7d82674f8
Álvaro Bergantiños <alvaro.bergantinos@lvis.es>
2026-08-28 13:43:35 +0200

    chore: scaffold Eleventy project structure
    Set up the initial Eleventy build pipeline to replace the static
    GitHub Pages site: add @11ty/eleventy dependency, .eleventy.js config
    (src -> _site, _includes/_data dirs, njk engine), and a minimal
    src/index.njk + base layout to validate the build. Ignore _site/ and
    .cache/ in git.

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function (eleventyConfig) {
+  // Copy static assets as-is to the output folder
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site",
+    },
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+  };
+};
